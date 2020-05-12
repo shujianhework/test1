15093141ff46b438f1ff988d9907294a39fc9724
sjh <shujianhe_qzzh@163.com>
2020-05-12 23:39:01 +0800

    1

---
+++ HashEngineering/IDEBackground/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.IDEBackground
+{
+    static class GuidList
+    {
+        public const string guidIDEBackgroundPkgString = "f144a5be-ed19-4e58-ba2c-d9375c64ad86";
+        public const string guidIDEBackgroundCmdSetString = "69452767-2529-4f4c-86fb-b8f448ac5a65";
+
+        public static readonly Guid guidIDEBackgroundCmdSet = new Guid(guidIDEBackgroundCmdSetString);
+    };
+}
---
+++ HashEngineering/IDEBackground/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IDEBackground")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Company")]
+[assembly: AssemblyProduct("IDEBackground")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("IDEBackground_IntegrationTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010091ae14b4ff8763470a534ee780371b23e32750f139f0bbc5544a9bcb3b77e396455a6a28a9a528450dd30379aedef18c5de65240d06de4e4c478900e8bcd3296e538b86ec539e3b353936b30e6e5167971ef156d9d272ee2caa74b019c3dddb3ef98f26c8e4db0521cdd898fd758b62dbea0f0eae18eac90f84682d02d2c04cd")]
+[assembly: InternalsVisibleTo("IDEBackground_UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010091ae14b4ff8763470a534ee780371b23e32750f139f0bbc5544a9bcb3b77e396455a6a28a9a528450dd30379aedef18c5de65240d06de4e4c478900e8bcd3296e538b86ec539e3b353936b30e6e5167971ef156d9d272ee2caa74b019c3dddb3ef98f26c8e4db0521cdd898fd758b62dbea0f0eae18eac90f84682d02d2c04cd")]
